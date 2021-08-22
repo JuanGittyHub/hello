9ac5f56ba2e917c6f3730d436f7a9162f63a0c27
Mbuso Kotobe <mbuso.kotobes@gmail.com>
2021-08-22 18:09:41 +0200

    Fulfilling Juans Wish

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,6 @@
 public class Main {
     public static void main (String args[])
     {
-        System.out.println("Hello \n 
-        To change this license header, choose License Headers in Project Properties.");
+        System.out.println("Hello \n To change this template file, choose Tools \n To change this license header, choose License Headers in Project Properties.");
     }
 }
